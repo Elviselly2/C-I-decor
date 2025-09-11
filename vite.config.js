45ef11bedb96eea138b8a31b2dd6f7d544e47b1a
nosmall <elvisotieno24@gmail.com>
2025-09-11 12:38:50 +0300

    adds

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // This allows Vite to expose the host
+    hmr: {
+        clientPort: 443
+    },
+    preview: {
+      allowedHosts: [
+        'c-i-decor-9.onrender.com', 'https://c-i-decor-9.onrender.com/', // Add your Render URL here
+        'localhost',
+      ],
+    },
+  },
+});
