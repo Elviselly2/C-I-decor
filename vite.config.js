15c895b16530ef1ec617fb0214419c766217496c
nosmall <elvisotieno24@gmail.com>
2025-09-11 18:40:44 +0300

    Fix Vite allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Get the hostname from the environment, defaulting to localhost
+const hostname = process.env.VITE_APP_HOST || 'localhost';
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // This allows Vite to expose the host
+    host: true,
     hmr: {
-        clientPort: 443
+      clientPort: 443
     },
     preview: {
       allowedHosts: [
-        'c-i-decor-11.onrender.com', 
-        'localhost',
+        hostname, // Use the variable here
       ],
     },
   },
